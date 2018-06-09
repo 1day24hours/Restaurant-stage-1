c2299707e4f1f2659a6737bf26e590d4aefca41a
Felix Chen <chen.felix.xi@gmail.com>
2018-06-09 10:52:48 +0800

    v-3.1 modified a sw minor issue

--- sw.js
+++ sw.js
@@ -49,7 +49,7 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys => {
             return Promise.all(
-                key.map(key => {
+                keys.map(key => {
                     if (key != cacheName) {
                         return caches.delete(key);
                     }
